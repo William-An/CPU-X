a9326ecce1944ad33de76762d33e4703114984e5
William An <china_aisa@live.com>
2022-06-08 00:12:33 -0400

    Add license info

--- asm/util.h
+++ asm/util.h
@@ -5,10 +5,39 @@
 * @email:     an107@purdue.edu
 * @version:   v1.0.0
 * @date:      12/27/2021
-* @brief:     Utility macros for unit testing, inspired from
-*             riscv-tests repo
+* @brief:     Utility macros for unit testing, based
+*             on the riscv-tests repo 
+*             (https://github.com/riscv-software-src/riscv-tests)
 *
 ************************************************************/
+
+/**
+ * Copyright (c) 2012-2015, The Regents of the University of California (Regents).
+ * All Rights Reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Regents nor the
+ *    names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ * 
+ * IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+ * SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING
+ * OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF REGENTS HAS
+ * BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
+ * HEREUNDER IS PROVIDED "AS IS". REGENTS HAS NO OBLIGATION TO PROVIDE
+ * MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ **/
+
 
 // Address to write the final test result, whether is zero (passed)
 // or not (contains the failed test number)
@@ -81,6 +110,9 @@ init_xreg:                                                                  \
 #define INSERT_NOPS_10 nop; INSERT_NOPS_9
 
 
+// Test num, result register, expected value, code to produce the actual value
+// Will compare the result register with expected value and branch to test_fail 
+// To write the failed test number to register r3 (gp) and enter deadloop
 #define TEST_CASE(test_num, result_reg, exp, code...)   \
 test_ ## test_num:                                                          \
     code;                                                                   \
