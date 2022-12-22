8daa8a12d354189fe0ef0a00dd97158e3c6ccd66
William An <china_aisa@live.com>
2022-12-21 19:10:52 -0500

    Update testbench to stop on test finish

--- testbench/tb_system.sv
+++ testbench/tb_system.sv
@@ -6,8 +6,8 @@ module tb_system;
     import rv32ima_pkg::*;
 	
     localparam CLK_PERIOD = 10;
-	 // localparam PC_INIT = 32'h80000000;
-	 localparam PC_INIT = -4;
+    // localparam PC_INIT = 32'h80000000;
+    localparam PC_INIT = -4;
     logic tb_clk;
     logic tb_nrst;
     word_t tb_data;
@@ -28,12 +28,23 @@ module tb_system;
         repeat (3) @(posedge tb_clk);
         tb_nrst = 1'b1;
 
+        // Break on ecall and 50 clock cycles
+        // Then check x3 register value
         // Break on if x30 and x31 contains value 0xBEEFBEEF
-        while (!(dut.dp0.rf0.rf[30] == 32'hbeefbeef && dut.dp0.rf0.rf[31] == 32'hbeefbeef)) begin
+        while (!(dut.dp0.exceptionif0.dec_exception_event.ecall == 1'b1)) begin
             @(posedge tb_clk);
             tb_cycles++;
         end
+        repeat (50) @(posedge tb_clk);
+        if (dut.dp0.rf0.rf[3] == 32'd1) begin
+            $display("All test passed!");
+        end
+        else begin
+            $error("Test failed! Code: %d", dut.dp0.rf0.rf[3]);
+        end
         $stop;
+
+        // TODO tohost memory?
         // TODO Dump memory
     end
 endmodule
